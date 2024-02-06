0152e75daefea0427988742ec64b6615208998a3
Vivek-4321 <vivekvenugopal514@gmail.com>
2024-02-06 21:11:17 +0530

    Multi Step Form Challenge added

--- Challenges/src/App.tsx
+++ Challenges/src/App.tsx
@@ -5,6 +5,7 @@ import Counter from './Counter'
 import ProgressBar from './ProgressBar'
 import FileExplorer from './FileExplorer';
 import DependentDropDown from './DependentDropDown';
+import MultiStepForm from './MultiStepForm/MultiStepForm';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/progress-bar" element={<ProgressBar />} />
           <Route path='/file-explorer' element={<FileExplorer />} />
           <Route path='/dropdown' element={<DependentDropDown/>} />
+          <Route path='/multi-step-form' element={<MultiStepForm />} />
         </Routes>
       </BrowserRouter>
     </>
