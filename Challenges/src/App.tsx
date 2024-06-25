04bdbb05ced12fe5f5c6d3400e555682e8aad45b
Vivek-4321 <vivekvenugopal514@gmail.com>
2024-06-25 18:48:25 +0530

    Browser router changed.

--- Challenges/src/App.tsx
+++ Challenges/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home.tsx";
 import Navbar from "./Navbar.tsx";
 import Counter from "./counter/Counter.tsx";
@@ -8,7 +8,7 @@ import CanvasApp from "./canvas/CanvasApp.tsx";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -16,7 +16,7 @@ function App() {
           <Route path="/bmi" element={<BMICalculator />} />
           <Route path="/canvas" element={<CanvasApp/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
